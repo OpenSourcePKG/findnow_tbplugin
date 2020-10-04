24366c484940e54fc1916a48e23586b2a9926f0e
Stefan Werfling <stefan.werfling@pegenau.de>
2020-10-04 12:25:37 +0200

    update part 3

--- dist/chrome/scripts/background.js
+++ dist/chrome/scripts/background.js
@@ -1,5 +1,5 @@
 "use strict";
 
 browser.findnow.init();
-//browser.composeAction.onClicked.addListener(function() { browser.findnow.click(); });
+browser.composeAction.onClicked.addListener(function() { browser.findnow.click(); });
 browser.runtime.onUpdateAvailable.addListener(function() { /* null */ });
