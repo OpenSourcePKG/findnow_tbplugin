361c9350fb605b2a4bcce7e8e83a88abcc768e83
Stefan Werfling <stefan.werfling@pegenau.de>
2020-11-19 14:46:58 +0100

    update options save

--- dist/chrome/content/ui/options.js
+++ dist/chrome/content/ui/options.js
@@ -10,11 +10,11 @@ const FNOptions = {
         document.getElementById('defaultButton').checked = await findnow.getPref('bool', 'button_show_default');
         document.getElementById('addtimeCheckbox').checked = await findnow.getPref('bool', 'export_filenames_addtime');
 
-        if( await findnow.getPref('type',  "exportEML_dir") > 0 ) {
-            document.getElementById("export_eml_dir").value = await findnow.getPref('string', "exportEML_dir");
+        if( await findnow.getPref('type',  "export_eml_dir") > 0 ) {
+            document.getElementById("export_eml_dir").value = await findnow.getPref('string', "export_eml_dir");
         }
 
-        if( await findnow.getPref('bool', "exportEML_use_dir") ) {
+        if( await findnow.getPref('bool', "export_eml_use_dir") ) {
             document.getElementById("use_export_eml_dir").checked = true;
 
             document.getElementById("export_eml_dir").removeAttribute("disabled");
@@ -29,11 +29,11 @@ const FNOptions = {
 
         // ---------------------------------------------------------------------
 
-        if( await findnow.getPref('type',  "exportEML_sub_dir") > 0 ) {
-            document.getElementById("export_eml_sub_dir").value = await findnow.getPref('string', "exportEML_sub_dir");
+        if( await findnow.getPref('type',  "export_eml_sub_dir") > 0 ) {
+            document.getElementById("export_eml_sub_dir").value = await findnow.getPref('string', "export_eml_sub_dir");
         }
 
-        if( await findnow.getPref('bool', "exportEML_use_sub_dir") ) {
+        if( await findnow.getPref('bool', "export_eml_use_sub_dir") ) {
             document.getElementById("use_export_eml_sub_dir").checked = true;
             document.getElementById("export_eml_sub_dir").removeAttribute("disabled");
         }
@@ -100,22 +100,20 @@ const FNOptions = {
         document.getElementById("commonsave").onclick = async function() {
             await findnow.setPref('bool', 'button_show_default', document.getElementById("defaultButton").checked);
             await findnow.setPref('bool', 'export_filenames_addtime', document.getElementById("addtimeCheckbox").checked);
-            await findnow.setPref('bool', 'exportEML_use_dir', document.getElementById("use_export_eml_dir").checked);
+            await findnow.setPref('bool', 'export_eml_use_dir', document.getElementById("use_export_eml_dir").checked);
 
-            if( document.getElementById("export_eml_dir").value !== "" ) {
-                await findnow.setPref(
-                    'string',
-                    'exportEML_dir',
-                    document.getElementById("export_eml_dir").value
-                );
+            const exportemldir = document.getElementById("export_eml_dir").value;
+
+            if( exportemldir !== "" ) {
+                await findnow.setPref('string', 'export_eml_dir', exportemldir);
             }
 
-            await findnow.setPref('bool', 'exportEML_use_sub_dir', document.getElementById("use_export_eml_sub_dir").checked);
+            await findnow.setPref('bool', 'export_eml_use_sub_dir', document.getElementById("use_export_eml_sub_dir").checked);
 
             if( document.getElementById("export_eml_sub_dir").value !== "" ) {
                 await findnow.setPref(
                     'string',
-                    'exportEML_sub_dir',
+                    'export_eml_sub_dir',
                     document.getElementById("export_eml_sub_dir").value
                 );
             }
